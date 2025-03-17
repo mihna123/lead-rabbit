4aa2550c2788ecd49e39e44b7bd2ec1ebc702fb3
mihna123 <mihailonvojinovic@gmail.com>
2025-03-17 17:37:40 +0100

    Add google OAuth and move files to correct directory

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import SignInButton from "@/components/buttons/sign-in";
 import SignOutButton from "@/components/buttons/sign-out";
-import { auth } from "../../auth";
+import { auth } from "@/auth";
 
 export default async function Home() {
 	const session = await auth();
 	return (
 		<div>
-			Helloooo {session?.user?.email}{" "}
+			Helloooo {session?.user?.name?.split(" ")[0]}{" "}
 			{!session?.user ? <SignInButton /> : <SignOutButton />}
 		</div>
 	);
