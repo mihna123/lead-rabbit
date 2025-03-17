4d5a1c3788817de1703262ac9bc185469002e613
mihna123 <mihailonvojinovic@gmail.com>
2025-03-17 17:12:05 +0100

    Add some user info to the landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,13 @@
-export default function Home() {
-	return <div>Helloooo</div>;
+import SignInButton from "@/components/buttons/sign-in";
+import SignOutButton from "@/components/buttons/sign-out";
+import { auth } from "../../auth";
+
+export default async function Home() {
+	const session = await auth();
+	return (
+		<div>
+			Helloooo {session?.user?.email}{" "}
+			{!session?.user ? <SignInButton /> : <SignOutButton />}
+		</div>
+	);
 }
