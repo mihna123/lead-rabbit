b9555c09576ba4753f050e6fb190894232887797
mihna123 <mihailonvojinovic@gmail.com>
2025-03-17 19:11:40 +0100

    Connect the database

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import SignInButton from "@/components/buttons/sign-in";
 import SignOutButton from "@/components/buttons/sign-out";
 import { auth } from "@/auth";
+import client from "@/lib/mongodb";
 
 export default async function Home() {
 	const session = await auth();
+	const users = await client.db().collection("users").find().toArray();
 	return (
 		<div>
 			Helloooo {session?.user?.name?.split(" ")[0]}{" "}
 			{!session?.user ? <SignInButton /> : <SignOutButton />}
+			<p>{JSON.stringify(users)}</p>
 		</div>
 	);
 }
