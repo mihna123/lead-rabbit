9a167ffa037ce43a7440b3ec10bb4323c0fa1e32
mihna123 <mihailonvojinovic@gmail.com>
2025-03-18 18:59:05 +0100

    Move fonts to separate module

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
+import { grandstander } from "@/fonts";
 
 export const metadata = {
 	title: "LeadRabbit",
@@ -19,9 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={`antialiased ${grandstander.className}`}>
 				{children}
 			</body>
 		</html>
---
+++ src/fonts.js
@@ -0,0 +1,17 @@
+import { Gamja_Flower, Grandstander, Kode_Mono } from "next/font/google";
+
+export const gamjaFlower = Gamja_Flower({
+	subsets: ["latin"],
+	weight: "400",
+	variable: "--gamja-flower",
+});
+
+export const kodeMono = Kode_Mono({
+	subsets: ["latin"],
+	variable: "--kode-mono",
+});
+
+export const grandstander = Grandstander({
+	subsets: ["latin"],
+	variable: "--grandstander",
+});
