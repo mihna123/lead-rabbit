90a0ff5b41d9bcf04a41fde469c3e64443acf917
mihna123 <mihailonvojinovic@gmail.com>
2025-03-20 20:02:02 +0100

    Add session protection to edit page

--- src/app/edit/page.js
+++ src/app/edit/page.js
@@ -2,13 +2,20 @@
 
 import Header from "@/components/header";
 import EditCTAForm from "@/components/forms/edit-cta-form";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 
 export default async function EditPage() {
+	const session = await auth();
+	if (!session?.user) {
+		redirect("/");
+	}
+
 	return (
 		<div>
 			<Header />
 			<div className="flex justify-center">
-				<EditCTAForm />
+				<EditCTAForm session={session} />
 			</div>
 		</div>
 	);
