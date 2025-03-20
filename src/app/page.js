47525b922856eee16ed5ac90fd1a6a0e3720b7d3
mihna123 <mihailonvojinovic@gmail.com>
2025-03-20 20:03:00 +0100

    Remove logo from app page

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import SignInButton from "@/components/buttons/sign-in";
 import SignOutButton from "@/components/buttons/sign-out";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-import LeadRabbitLogo from "@/components/lead-rabbit-logo";
 
 export default async function Home() {
 	const session = await auth();
@@ -11,7 +10,6 @@ export default async function Home() {
 	}
 	return (
 		<div>
-			<LeadRabbitLogo />
 			Helloooo {session?.user?.name?.split(" ")[0]}{" "}
 			{!session?.user ? <SignInButton /> : <SignOutButton />}
 		</div>
