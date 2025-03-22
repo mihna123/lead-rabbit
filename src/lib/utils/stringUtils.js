c92c32949357bb07261ef58a54c2476f835a3e51
mihna123 <mihailonvojinovic@gmail.com>
2025-03-22 18:47:39 +0100

    Add tryParseJson method to utils

--- src/lib/utils/stringUtils.js
+++ src/lib/utils/stringUtils.js
@@ -4,3 +4,16 @@
 export function firstLetterUppercase(text) {
 	return text[0].toUpperCase() + text.slice(1, undefined);
 }
+
+/**
+ * @param {String} str
+ * */
+export function tryParseJson(str) {
+	try {
+		const o = JSON.parse(str);
+
+		if (o && typeof o === "object") {
+			return o;
+		}
+	} catch {}
+}
