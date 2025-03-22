0b84b0e298cf075ba72505ed970b7928362e77b3
mihna123 <mihailonvojinovic@gmail.com>
2025-03-22 15:38:43 +0100

    Add state to signout button

--- src/components/buttons/sign-out.js
+++ src/components/buttons/sign-out.js
@@ -1,11 +1,23 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { useState } from "react";
 
 export default function SignOutButton() {
+	const [isPending, setIsPending] = useState(false);
+
+	const handleButtonClick = () => {
+		setIsPending(true);
+		signOut();
+	};
 	return (
-		<button type="button" onClick={() => signOut()}>
-			Sign Out
+		<button
+			className="cursor-pointer w-fit"
+			type="button"
+			onClick={handleButtonClick}
+			disabled={isPending}
+		>
+			{isPending ? "Signing Out..." : "Sign Out"}
 		</button>
 	);
 }
