5543c8a295fe63c019f1b592959aef8e0ca66f67
mihna123 <mihailonvojinovic@gmail.com>
2025-03-23 19:34:07 +0100

    Add trustpilot

---
+++ src/components/trustpilot-widget.js
@@ -0,0 +1,20 @@
+export function TrustpilotWidget() {
+	return (
+		<div
+			class="trustpilot-widget"
+			data-locale="en-US"
+			data-template-id="56278e9abfbbba0bdcd568bc"
+			data-businessunit-id="67e0508d9821aeb44d3f06dd"
+			data-style-height="52px"
+			data-style-width="100%"
+		>
+			<a
+				href="https://www.trustpilot.com/review/lead-rabbit.com"
+				target="_blank"
+				rel="noopener"
+			>
+				Trustpilot
+			</a>
+		</div>
+	);
+}
