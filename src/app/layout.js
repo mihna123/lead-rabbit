b67e4f3d3ed36ecbdf5751998a188e69ac7d4f3e
mihna123 <mihailonvojinovic@gmail.com>
2025-03-24 16:14:25 +0100

    Remove the trustpilot

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
-import { TrustpilotWidget } from "@/components/trustpilot-widget";
 import "./globals.css";
 import { grandstander } from "@/fonts";
-import Script from "next/script";
 
 export const metadata = {
 	title: "LeadRabbit",
@@ -11,16 +9,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<Script
-				strategy="lazyOnload"
-				type="text/javascript"
-				src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
-				async
-			/>
-
 			<body className={`antialiased ${grandstander.className}`}>
 				{children}
-				<TrustpilotWidget />
 			</body>
 		</html>
 	);
